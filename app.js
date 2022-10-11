3b7244faba9639aa197d382dfd0860584ed04f76
Dinosauriopan <agomezh2003@alumno.ipn.mx>
2022-10-10 23:00:18 -0500

    nada

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mysql = require('mysql2')
 let app = express()
 
 let bodyParser = require('body-parser')
-const { query } = require('express')
 let con = mysql.createConnection({
 
     host: 'localhost',
